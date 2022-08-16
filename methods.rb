534bc4a3b056366b618f7d91ebdec42e0f49e461
Judy Mwai <juddiemukami@gmail.com>
2022-08-16 17:03:21 +0300

    commit tree 1996259aedc23adcb9ea0afe8a1d99b48a6b3c2b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,43 @@
 # Your code here!
+def  greet_programmer
+
+    puts "Hello, programmer!"
+   
+end
+
+greet_programmer
+
+def greet (name)
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+
+
+def add (num1=1, num2=2)
+     return num1 + num2
+end
+add()
+
+# def greet_with_default (name="Naureen")
+#     puts "Hello, #{name}!"
+# end
+# greet_with_default()
+
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+
+
+
+def halve (name)
+     if name.class != Integer
+        return nil
+     else 
+        return name /2
+     end
+end 
+puts halve(6)
+
+
